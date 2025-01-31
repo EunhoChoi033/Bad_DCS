afe238dcacab9067c3318466e1e0cc6ca88ddaf6
EunhoChoi033 <eunhochoi033@gmail.com>
2025-01-31 15:25:36 -0800

    Fixed Missile unable to/losing track

--- missile.cpp
+++ missile.cpp
@@ -10,6 +10,7 @@ Missile::Missile(Vector2 position, float speed, int id) {
     image.height /= 4;
     rotation = 0.0f;
     active = true;
+    direction = {0, 0};
 }
 
 Missile::Missile() {
@@ -23,6 +24,13 @@ void Missile::Draw() {
 }
 
 void Missile::Update(Vector2 targetPos, float targetWidth, float targetHeight) {
+    
+    if (active) {
+        if (((position.x + image.width) < 0) || (position.x > GetScreenWidth())) {
+            active = false;
+        }
+    }
+    
     if ((position.y - 250) > targetPos.y) {
         direction = {targetPos.x + (targetWidth / 2) - position.x - (image.width / 2), targetPos.y + (targetHeight / 2) - position.y - (image.height / 2)};
         normalizeVector();
@@ -32,11 +40,16 @@ void Missile::Update(Vector2 targetPos, float targetWidth, float targetHeight) {
         position.x += velocity.x;
         position.y += velocity.y;
     } else {
-        Vector2 velocity = {direction.x * speed, direction.y * speed};
-        position.x += velocity.x;
-        position.y += velocity.y;
-        rotation = atan2((double)direction.y, (double)direction.x);
-        cout << "No Tracking" << endl;
+        Vector2 velocity;
+        if (direction.x == 0 && direction.y == 0) {
+            position.y -= speed;
+            rotation = atan(-90);
+        } else {
+            velocity = {direction.x * speed, direction.y * speed};
+            position.x += velocity.x;
+            position.y += velocity.y;
+            rotation = atan2((double)direction.y, (double)direction.x);
+        }
     }
 }
 
